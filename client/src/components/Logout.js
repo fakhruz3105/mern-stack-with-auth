c2ff54d36d9432204fbb593d7a8aeca40ff447a3
fakhruz3105 <fakhruz3105@gmail.com>
2020-02-16 18:57:04 +0800

    edit

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,17 +1,27 @@
 import React from "react";
 import { logoutAction } from "../actions/authActions";
 import { useSelector, useDispatch } from "react-redux";
-import { NavLink } from "reactstrap";
+import { NavItem, NavLink } from "reactstrap";
 
 export default function Logout() {
   const dispatch = useDispatch();
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+  const user = useSelector(state => state.auth.user);
 
   const content = (
     <>
       <NavLink
+        onClick={null}
+        href="#"
+        className="navLink"
+        style={isAuthenticated ? null : { display: "none" }}
+      >
+        {user ? `Welcome, ${user.name}` : null}
+      </NavLink>
+      <NavLink
         onClick={() => dispatch(logoutAction())}
         href="#"
+        className="navLink"
         style={isAuthenticated ? null : { display: "none" }}
       >
         Logout
